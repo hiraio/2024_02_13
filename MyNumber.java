4131fb34c1c0883684be6b11ebc249a02e9a8a1b
jun_v <159228500+hiraio@users.noreply.github.com>
2024-02-13 17:10:26 +0900

    Add files via upload

--- MyNumber.java
+++ MyNumber.java
@@ -1,4 +1,4 @@
-package edu.java.access03;
+package edu.java.access07;
 
 public class MyNumber {
  // 멤버 변수
@@ -20,5 +20,16 @@ public class MyNumber {
 	public void setNumber(int number) {
 		this.number = number ;
 	}
+	
+	// 기능 : 현재 객체(this : MyNumber)의 number 변수와
+	//		매개변수로 받아온 객체의  number를 더한 후 
+	// 		현재 객체의 number에 저장
+	// 리턴타입: MyNumber 
+	// 함수 이름 : add
+	// 매개변수 : MyNumber number
+	public MyNumber add(MyNumber number) {
+		this.number = this.number + number.number;
+		return this;
+	}
 
-}
+} // end MyNumber
