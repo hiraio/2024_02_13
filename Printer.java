aaa7a45a8f71845695ca813b9fca0d41a4b14afc
jun_v <159228500+hiraio@users.noreply.github.com>
2024-02-13 12:23:16 +0900

    Add files via upload

---
+++ Printer.java
@@ -0,0 +1,24 @@
+package edu.java.homework;
+
+public class Printer {
+
+public void println(int result) {
+	System.out.println(result);
+	
+}
+
+public void println(double result) {
+	System.out.println(result);
+	
+}
+
+public void println(boolean result) {
+	System.out.println(result);
+	
+}
+
+public void println(String result) {
+	System.out.println(result);
+	
+}
+} // end Printer
